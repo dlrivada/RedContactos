02f1e65a3eff18e8d3d6a6ce58b4aea4a0b94d52
David Lozano Rivada <dlrivada@hotmail.com>
2016-01-20 14:32:18 +0100

    20/01/2016

---
+++ MvvmLibrary2/ModulosBase/AutofacBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using MvvmLibrary2.Factories;
+
+namespace MvvmLibrary2.ModulosBase
+{
+    public abstract class AutofacBootstrapper
+    {
+        public void Run()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            ConfigureContainer(builder);
+            IContainer container = builder.Build();
+            IViewFactory viewFactory = container.Resolve<IViewFactory>();
+            RegisterViews(viewFactory);
+            ConfigureApplication(container);
+        }
+
+        protected virtual void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule<AutofacModule>();
+        }
+
+        protected abstract void RegisterViews(IViewFactory viewFactory);
+        protected abstract void ConfigureApplication(IContainer container);
+
+    }
+}
---
+++ MvvmLibrary2/ModulosBase/AutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using MvvmLibrary2.Factories;
+using Xamarin.Forms;
+
+namespace MvvmLibrary2.ModulosBase
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ViewFactory>().As<IViewFactory>().SingleInstance();
+            builder.RegisterType<Navigator>().As<INavigator>().SingleInstance();
+            //builder.Register(ctx => Application.Current.MainPage.Navigation).SingleInstance();
+        }
+    }
+}
